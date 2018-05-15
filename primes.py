279ac1c2b6a960ad92ca620644111da555b29007
windjacker <jan.wessling@mailbox.tu-dresden.de>
2018-05-15 19:53:52 +0200

    correct prime calculation
    and some correct primes

--- primes.py
+++ primes.py
@@ -1,31 +1,41 @@
 import time
 from datetime import datetime
 
-inp = input("Geplante Berechnungszeit in min: ");
-dt = float(inp)
 
+def read_data(a):
+    data = open('Primzahlen.txt', 'r')
 
-data = file('Primzahlen.txt', 'r+w')
+    for i in data:
+        a.append(int(i[:-1]))
+    
+    data.close()
+
+def calculation(a):
+    inp = input("Geplante Berechnungszeit in min: ");
+    dt = float(inp)
+    data = open('Primzahlen.txt', 'a')
+
+    t = time.time();
+    i = a[-1]+2
+    print("Start: {:%H:%M:%S} - {}".format(datetime.now(), i) )
+    while ( time.time()-t <= dt*60):
+        isPrime = True
+        for p in a:
+            if i%p==0:
+                isPrime = False
+                break
+            if p**2>i:
+                break
+        if isPrime:
+            a.append(i)
+            data.write( str(i) + '\n' )
+        i += 2
+
+    print("Stop : {:%H:%M:%S} - {}".format(datetime.now(), i) )
+
 
 primarr = []
-for i in data:
-    primarr.append(int(i[:-1]))
+read_data(primarr)
 
-
-t = time.time();
-i = primarr[-1]+2
-print("Start: {:%H:%M:%S} - {}".format(datetime.now(), i) )
-while ( time.time()-t <= dt*60):
-    isPrime = True
-    for p in primarr:
-        if i%p==0:
-            isPrime = False
-            break
-        if p**2>i:
-            break
-    if isPrime:
-        primarr.append(i)
-        data.write( str(i) + '\n' )
-    i += 2
-
-print("Stop : {:%H:%M:%S} - {}".format(datetime.now(), i) )
+if __name__ == '__main__':
+    calculation(primarr)
